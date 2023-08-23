a6297201fa45f8e3eded2beb430d4fb096e74850
kosumi <chankocyo@gmail.com>
2023-08-23 09:45:43 -0600

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Xv6";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; 
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils,... }: let
+    lib = {
+      inherit (flake-utils.lib) defaultSystems eachSystem;
+    };
+    supportedSystems = [ "x86_64-linux" ];
+  in lib.eachSystem supportedSystems (system: let
+    nightlyVersion = "2023-01-15";
+    pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          # (import rust-overlay)
+          #(import ./pkgs)
+        ];
+      };
+    # pinnedRust = pkgs.rust-bin.nightly.${nightlyVersion}.default.override {
+    #   extensions = ["rustc-dev" "rust-src" "rust-analyzer-preview" ];
+    #   targets = [ "x86_64-unknown-linux-gnu" ];
+    # };
+    # rustPlatform = pkgs.makeRustPlatform {
+    #   rustc = pinnedRust;
+    #   cargo = pinnedRust;
+    # };
+    #cargoPlay = pkgs.cargo-feature.override { inherit rustPlatform; };
+  in {
+    
+devShell = pkgs.pkgsCross.riscv64.mkShell rec {
+  nativeBuildInputs = with pkgs; [
+    qemu
+  ];
+  buildInputs = with pkgs; [
+
+  ];
+
+  shellHook = ''
+  '';
+};
+
+  });
+}
