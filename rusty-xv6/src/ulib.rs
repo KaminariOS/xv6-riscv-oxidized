02c5efba7e92c8168f0b37e5258c401ee9ec3936
kosumi <chankocyo@gmail.com>
2023-08-26 00:13:14 -0600

    Found a bug in Rustc: if opt-level != 0, compilation failure; core::slice makes binary size almost 1M

--- rusty-xv6/src/ulib.rs
+++ rusty-xv6/src/ulib.rs
@@ -1,15 +1,21 @@
 #[allow(non_camel_case_types)]
 type int = i32;
+#[allow(non_camel_case_types)]
+type uint = u32;
 
 #[allow(dead_code)]
 extern "C" {
     pub fn printf(s: *const u8, args: ...);
+    pub fn fprintf(_: int, s: *const u8, args: ...);
     pub fn exit(status: int) -> int;
     pub fn fork() -> int;
-    pub fn pipe(_: *mut int) -> int;
+    pub fn pipe(_: *mut [int; 2]) -> int;
     pub fn wait(_: *mut int) -> int;
-    pub fn close(_: *mut int) -> int;
+    pub fn close(_: int) -> int;
+    pub fn dup(_: int) -> int;
     pub fn uptime() -> int;
+    pub fn malloc(s: uint) -> *mut u8;
+    pub fn read(_: int, _: *mut u8, _: int);
 }
 
 #[macro_export]
