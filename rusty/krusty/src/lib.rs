970649c7bd22392fbef2054e0ed6548bb73bdb99
kosumi <chankocyo@gmail.com>
2023-09-20 20:47:30 -0600

    Add allocator

--- rusty/krusty/src/lib.rs
+++ rusty/krusty/src/lib.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![feature(panic_info_message)]
+#![feature(alloc_error_handler)]
 
 #[macro_use]
 pub mod console;
@@ -9,6 +10,10 @@ mod ring;
 
 use kernelib::*;
 pub use ring::sys_ring;
+
+use linked_list_allocator::LockedHeap;
+#[global_allocator]
+static ALLOCATOR: LockedHeap = LockedHeap::empty();
 
 #[no_mangle]
 pub unsafe extern "C" fn rust_function(ptr: u64) {
