e2bd64cd992306ee316aa0c5e9055b70bbc8f603
kosumi <chankocyo@gmail.com>
2023-09-25 19:27:17 -0600

    Add global alloctor to user space

--- rusty/rusty-xv6/src/bin/rb.rs
+++ rusty/rusty-xv6/src/bin/rb.rs
@@ -2,19 +2,23 @@
 #![no_main]
 
 use log::*;
-use user_lib::*;
+use user_lib::{*, string::*};
 use user_lib::user_ring::*;
 
 #[no_mangle]
 pub extern "C" fn main(_argc: i32, _argv: *const *const u8) -> i32 {
-    logger::init().unwrap();
     info!("Test log");
     error!("Test log");
     debug!("Test log");
     trace!("Test log");
     warn!("Test log");
+    // let test = String::new("test");
     let pid = fork();
     let test_times = 50000000;
+
+    // let string = String::from("Test heap string");
+    // println!("{}", string);
+    
     if let Ok(index) = ringbuf("test\0", true) {
         if pid != 0 {
             // let () =
@@ -28,6 +32,7 @@ pub extern "C" fn main(_argc: i32, _argv: *const *const u8) -> i32 {
             let (len, base) = ringbuf_start_read(index);
             info!("Child: len: {} base: 0x{:x}", len, base);
         }
+
         assert_eq!(get_count(index), test_times * 2);
         ringbuf("test\0", false).unwrap();
     }
--- rusty/rusty-xv6/src/lib.rs
+++ rusty/rusty-xv6/src/lib.rs
@@ -2,6 +2,7 @@
 #![feature(linkage)]
 #![feature(panic_info_message)]
 #![feature(c_variadic)]
+#![feature(alloc_error_handler)]
 
 #[macro_use]
 pub mod console;
@@ -14,11 +15,36 @@ pub mod logger;
 
 pub use syscall::*;
 pub use user_ring::ringbuf;
+use linked_list_allocator::LockedHeap;
+
+const USER_HEAP_SIZE: usize = 32768;
+
+static mut HEAP_SPACE: [u8; USER_HEAP_SIZE] = [0; USER_HEAP_SIZE];
+
+#[global_allocator]
+static HEAP: LockedHeap = LockedHeap::empty();
+
+#[alloc_error_handler]
+pub fn handle_alloc_error(layout: core::alloc::Layout) -> ! {
+    panic!("Heap allocation error, layout = {:?}", layout);
+}
+
+
+extern crate alloc;
+use alloc::vec::Vec;
+pub use alloc::string;
 
 #[no_mangle]
 #[link_section = ".text.entry"]
 pub unsafe extern "C" fn _start() -> ! {
     // clear_bss();
+
+    unsafe {
+        HEAP.lock()
+            .init(HEAP_SPACE.as_ptr() as _, USER_HEAP_SIZE);
+    }
+
+    logger::init().unwrap();
     exit(main());
     panic!("unreachable after sys_exit!");
 }
